0f5529d0f98d8282b8054cf3276e49dc9b59b25f
Jonathan Arciaga <jonarciaga@gmail.com>
2020-03-24 16:50:25 -0500

    Added AddBuilding objects and views.

--- SatisfactoryCalculator/MainWindowViewModel.cs
+++ SatisfactoryCalculator/MainWindowViewModel.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using SatisfactoryCalculator.Helpers;
+using SatisfactoryCalculator.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SatisfactoryCalculator
 {
@@ -13,11 +16,49 @@ namespace SatisfactoryCalculator
 
         public MainWindowViewModel()
         {
+            OpenAddBuildingViewCommand = new RelayCommand(o => { OpenAddBuildingView(); }, o => true);
         }
 
         #endregion
 
         #region " Properties "
+
+        private object _currentViewModel;
+        public object CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set 
+            {
+                _currentViewModel = value;
+                OnPropertyChanged("CurrentViewModel");
+            }
+        }
+
+        private AddBuildingViewModel _addBuildingViewModel;
+        public AddBuildingViewModel AddBuildingViewModel
+        {
+            get { return _addBuildingViewModel; }
+            set 
+            { 
+                _addBuildingViewModel = value;
+                OnPropertyChanged("AddBuildingViewModel");
+            }
+        }
+
+        #region " Command Properties "
+
+        private ICommand _openAddBuildingViewCommand;
+        public ICommand OpenAddBuildingViewCommand
+        {
+            get { return _openAddBuildingViewCommand; }
+            set
+            {
+                _openAddBuildingViewCommand = value;
+                OnPropertyChanged("OpenAddBuildingViewCommand");
+            }
+        }
+
+        #endregion
 
         #endregion
 
@@ -26,6 +67,13 @@ namespace SatisfactoryCalculator
         #endregion
 
         #region " Commands "
+
+        private void OpenAddBuildingView()
+        {
+            Console.WriteLine("Add Building Command Initiated");
+            AddBuildingViewModel _addBuildingViewModel = new AddBuildingViewModel();
+            CurrentViewModel = _addBuildingViewModel;
+        }
 
         #endregion
 
